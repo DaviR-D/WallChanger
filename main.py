50cf26c6c7477000a2b1216589c80fb25d179393
DaviR-D <davirdavanzo@gmail.com>
2020-11-10 19:31:11 -0300

    UI implemented

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
+import subprocess
+import json
 import os
 
-class Comp(Gtk.Window):
+class WallChanger(Gtk.Window):
 	def __init__(self):
 		builder = Gtk.Builder()
 		builder.add_from_file('UI.glade')
@@ -11,17 +13,33 @@ class Comp(Gtk.Window):
 		self.switch = builder.get_object('switch')
 		self.chooser = builder.get_object('chooser')
 		self.startButton = builder.get_object('button')
+		self.cd_entry = builder.get_object('cd_entry')
+		self.window.connect("delete-event", Gtk.main_quit)
 		builder.connect_signals(self)
+		self.conf = dict()
+		with open("wallchanger.config") as config:
+			self.conf = json.load(config)
+		self.switch.set_state(self.conf['start_on_login'])
 
-	def onSwitch(self, state, data):
-		pass
+	def onSwitch(self, data, state):
+		self.conf['start_on_login'] = bool(state)
+		with open("wallchanger.config", 'w') as config:
+			json.dump(self.conf, config)
 
-	def onFolderSet():
-		pass
+	def setFolder(self, data):
+		self.conf['folder'] = self.chooser.get_filename()
+		with open("wallchanger.config", 'w') as config:
+			json.dump(self.conf, config)
 
-	def Start():
-		pass
 
-win = Comp()
+	def Start(self, data):
+		subprocess.run('python3 wallchanger.py &', shell=True)
+
+	def setCD(self, data):
+		self.conf['countdown'] = self.cd_entry.get_text()
+		with open("wallchanger.config", 'w') as config:
+			json.dump(self.conf, config)
+
+win = WallChanger()
 win.window.show()
 Gtk.main()
--- wallchanger.py
+++ wallchanger.py
@@ -1,12 +1,18 @@
 import os
 import glob
 import time
+import json
 import subprocess
 
-home = subprocess.check_output("echo $HOME", shell=True, universal_newlines=True)[:-1]
-wallpaper_list = glob.glob(home + '/Pictures/wall/*') # Wallpaper folder
+config = dict()
+
+with open("wallchanger.config") as conf:
+	config = json.load(conf)
+
+wallpaper_list = glob.glob(config['folder'] + '/*') # Wallpaper folder
 current_wallpaper = subprocess.check_output("gsettings get org.gnome.desktop.background picture-uri", shell=True, universal_newlines=True)[8:-2]
 index = wallpaper_list.index(current_wallpaper)
+countdown = config['countdown']
 
 while True:
 	for wallpaper in wallpaper_list[index::]:
