fe788ac54f0c17a5a99a82b9e6c2f4d18117d4be
DaviR-D <davirdavanzo@gmail.com>
2021-04-27 19:13:29 -0300

    Autostart feature implemented

--- Install.sh
+++ Install.sh
@@ -1,8 +1,13 @@
 if [ -d "$HOME/.local/share/WallChanger" ]; then
-    rm -r $HOME/.local/share/WallChanger && rm $HOME/.local/share/applications/WallChanger.desktop
+    rm -r $HOME/.local/share/WallChanger
+    rm $HOME/.local/share/applications/WallChanger.desktop
+    rm $HOME/.config/autostart/wcautostart.desktop
 fi
 
-echo Path=$HOME/.local/share/WallChanger >> WallChanger.desktop
 
-cp -r WallChanger $HOME/.local/share && cp WallChanger.desktop $HOME/.local/share/applications
+cp -r WallChanger $HOME/.local/share
+cp WallChanger.desktop $HOME/.local/share/applications
+cp wcautostart.desktop $HOME/.config/autostart
 
+echo Path=$HOME/.local/share/WallChanger >> $HOME/.local/share/applications/WallChanger.desktop
+echo Path=$HOME/.local/share/WallChanger >> $HOME/.config/autostart/wcautostart.desktop
